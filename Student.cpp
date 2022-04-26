1388d0839e811c866d05011419265f042cb7769e
yangyang guo <guo23188@qq.com>
2022-04-26 21:29:36 +0800

    实现Student父类

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by guo23 on 2022/4/26.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,22 @@
+//
+// Created by guo23 on 2022/4/26.
+//
+#pragma once
+#include<iostream>
+using namespace std;
+#ifndef CPPHOMEWORK_STUDENT_H
+#define CPPHOMEWORK_STUDENT_H
+
+
+class Student {
+public:
+    virtual void ShowInfo() = 0;
+    virtual string GetDepName() = 0;
+
+    int id;
+    string name;
+    int depId;
+};
+
+
+#endif //CPPHOMEWORK_STUDENT_H
