7aa8b65fa5163cc2a8d9c1ce45d8bc297cc32492
yangyang guo <guo23188@qq.com>
2022-04-29 19:18:32 +0800

    主函数main的注释

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 int main()
 {
     StudentManager sm;
+    //先新建一个对象
     /*
-     * 这是调试的代码
+     * 我是调试的代码
     Common *cm = NULL;
     cm = new Common(1,"yy",2);
     cm->ShowInfo();
@@ -21,39 +22,52 @@ int main()
     */
 
     int choice;
-
+    //接受用户的选择，做出相应的操作
     while(1)
     {
+        //死循环，可以一直接受数据
         sm.ShowMenu();
+        //让用户看看咱们的菜单，好点菜
         cout<<"请输入您的选择"<<endl;
+
         cin>>choice;
+        //接受用户的选择
         switch (choice) {
             case 0:
+                //退出系统，下次再见
                 cout<<"tuichu"<<endl;
                 sm.ExitSystem();
                 break;
             case 1:
+                //添加几个同学的信息
                 sm.AddStu();
                 break;
             case 2:
+                //展示当前添加进来的同学们的信息
                 sm.ShowStu();
                 break;
             case 3:
+                //删除一些同学的信息
                 sm.DelStu();
                 break;
             case 4:
+                //修改一些同学的信息
                 sm.ModStu();
                 break;
             case 5:
+                //查找一些同学的信息
                 sm.FindStu();
                 break;
             case 6:
+                //给同学们拍拍序
                 sm.SortStu();
                 break;
             case 7:
+                //清空文件
                 sm.CleanFile();
                 break;
             default:
+                //如果没有输入以上数字。清空重新打印菜单
                 system("cls");
                 break;
         }
